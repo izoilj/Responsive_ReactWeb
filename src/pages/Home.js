f6cd79a7a3b0d5260922f93ccb2d5c708fc2d997
izoilj <ljy9407@gmail.com>
2021-11-30 20:41:55 -0700

    Added InfoSection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection/HeroSection";
+import InfoSection from "../components/InfoSection/InfoSection";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
 
@@ -18,6 +19,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
+      <InfoSection />
     </>
   );
 };
